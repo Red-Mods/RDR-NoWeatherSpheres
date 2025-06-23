21755f82d1d3996265e3865e17fd15a4cf168637
K3rhos <30273537+K3rhos@users.noreply.github.com>
2025-06-23 21:12:55 +0200

    Update dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -8,7 +8,7 @@ BOOL APIENTRY DllMain(HMODULE _Module, DWORD _ReasonForCall, LPVOID _Reserved)
     {
         case DLL_PROCESS_ATTACH:
         {
-            // Prevent loading weather spheres, that mean that area like thieves landing will now longer feature an hardcoded gloomy weather.
+            // Prevent loading weather spheres, that mean that area like thieves landing will no longer feature an hardcoded gloomy weather.
             Pattern("48 85 DB 0F 84 93 ? ? ? 41 8B 45 34 C1 E8 14").Scan([](const Pattern& _This)
             {
                 _This.Add(4).Put(0x85);
